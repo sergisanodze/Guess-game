bd170aa8cebd6a0432ef6c91c00705e10b2ac4e4
Jopiol <you@example.com>
2023-11-23 00:00:26 +0400

    fixed bugs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//elements
+
 const button = document.getElementsByClassName('button')[0];
 const hearts = document.getElementsByClassName('fa-heart');
 const score = document.createElement("h1");
@@ -8,42 +10,52 @@ const answerText = document.createElement("h1");
 score.classList.add("score");
 scoreNumber.classList.add("score-number");
 
+// hearts & score 
+
 let remainingHearts = hearts.length;
 let scores = 0;
 let randomNum;
 
+// score elemenet
+
 document.body.appendChild(score);
-score.textContent="Score:"
+score.textContent="Score:";
 document.body.appendChild(scoreNumber);
-scoreNumber.textContent=`${scores}`
+scoreNumber.textContent=`${scores}`;
+
+// page refresh 
 
 const refreshPage = () => {
   location.reload();
 }
-refreshButton.addEventListener('click', refreshPage)
+refreshButton.addEventListener('click', refreshPage);
+
+// main code
 
 button.addEventListener('click',() =>{
     const min = 1;
-    const max = 20;
+    const max = 10;
     randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    guessButton.style.display = "block"
-    button.style.display = "none"
+    guessButton.style.display = "block";
+    button.style.display = "none";
 });
 
 guessButton.addEventListener('click',()=>{
-    const user = prompt("Guess the number from 1 to 20");
+    const user = prompt("Guess the number from 1 to 10");
 
     if(user == randomNum){
         alert("You've got it! :)");
         scores += 1;
         scoreNumber.textContent = `${scores}`;
         for (const heart of hearts){
-            while(remainingHearts !== 10){
+            if(remainingHearts !== 3){
                 remainingHearts++;
             }
             heart.style.color = "red";
+            guessButton.style.display = "none";
+            button.style.display = "block"
         }
-    }else if(user < 1 || user > 20){
+    }else if(user < 1 || user > 10){
         alert("please enter a number from 1 to 10");
     }else{
         alert("Nope :(");
@@ -51,6 +63,9 @@ guessButton.addEventListener('click',()=>{
             remainingHearts--;
             hearts[remainingHearts].style = "color:white;animation: play 1s ease;";
         }
+
+        // when you've run out of hearts
+
         if (remainingHearts === 0) {
             alert("You've run out of hearts! Game over.");
             button.disabled = true;
@@ -58,8 +73,8 @@ guessButton.addEventListener('click',()=>{
             button.style.display = "none";
             guessButton.style.display = "none";
             document.body.appendChild(answerText);
-            answerText.style = "display:block;color:white;"
-            answerText.textContent = `it was ${randomNum}`
+            answerText.style = "display:block;color:white;";
+            answerText.textContent = `it was ${randomNum}`;
         }
     }
 });
